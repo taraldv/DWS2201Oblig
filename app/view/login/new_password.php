291008d1b41a4c1141722b18cc7dd3d780dffd90
taraldv <36439345+taraldv@users.noreply.github.com>
2020-02-02 16:39:22 +0100

    Update new_password.php
    glemte bootstrap

--- app/view/login/new_password.php
+++ app/view/login/new_password.php
@@ -1,10 +1,16 @@
 <?php include VIEW.'header.php';?>
 <h1 class="display-1 text-center">Trenings logg</h1>
 <img class="mx-auto d-block" src="/img/logo.png">
-<form action='/login/update_password' method='post'>
-	<input type='password' name='password'>
-	<input type='submit' value='Send inn nytt passord'>
-	<input hidden name='token' value='<?php echo "$this->viewData" ?>'>
-</form>
+<div class='container'>
+	<form action='/login/update_password' method='post'>
+		<div class='form-group'>
+			<input class='form-control' type='password' name='password'>
+		</div>
+		<div class='form-group'>
+			<input class='form-control' type='submit' value='Send inn nytt passord'>
+			<input hidden name='token' value='<?php echo "$this->viewData" ?>'>
+		</div>
+	</form>
+</div>
 <?php include VIEW.'footer.php';?>
 
