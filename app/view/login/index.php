a9b9fae8b66d55b3c91b2dd38548040de282cf66
taraldv <36439345+taraldv@users.noreply.github.com>
2020-02-02 13:26:23 +0100

    Fikset indentering

--- app/view/login/index.php
+++ app/view/login/index.php
@@ -1,29 +1,29 @@
 <?php include VIEW.'header.php';?>
 <div class='container'>
-<form class='' action='/login/valid_login' method='post'>
-<div class="form-group">
-	<label for="email">Epost:</label>
-	<input class="form-control" type='text' name='email' id="email">
-</div>
-<div class="form-group">
-	<label for="password">Passord:</label>
-	<input class="form-control" type='password' name='password'>
-</div>
-<div class="form-group">
-	<input class='btn btn-primary' type='submit' value='Logg inn'>
-</div>
-<div class="form-group">
-	<a href='/login/register'>Registrer deg</a>
-</div>
-<div class="form-group">
-	<a href='/login/forgotten_password'>Glemt passord</a>
-</div>
-</form>
+	<form class='' action='/login/valid_login' method='post'>
+		<div class="form-group">
+			<label for="email">Epost:</label>
+			<input class="form-control" type='text' name='email' id="email">
+		</div>
+		<div class="form-group">
+			<label for="password">Passord:</label>
+			<input class="form-control" type='password' name='password'>
+		</div>
+		<div class="form-group">
+			<input class='btn btn-primary' type='submit' value='Logg inn'>
+		</div>
+		<div class="form-group">
+			<a href='/login/register'>Registrer deg</a>
+		</div>
+		<div class="form-group">
+			<a href='/login/forgotten_password'>Glemt passord</a>
+		</div>
+	</form>
 </div>
 <?php 
-if(!$this->viewData[0]){
-	echo "<h1>Feil epost,passord eller ikke bekreftet epost</h1>";
-}
+	if(!$this->viewData[0]){
+		echo "<h1>Feil epost,passord eller ikke bekreftet epost</h1>";
+	}
 ?>
 <?php include VIEW.'footer.php';?>
 
--- app/view/login/new_password.php
+++ app/view/login/new_password.php
@@ -1,11 +1,8 @@
-
 <?php include VIEW.'header.php';?>
-
 <form action='/login/update_password' method='post'>
-<input type='password' name='password'>
-<input type='submit' value='Send inn nytt passord'>
-<input hidden name='token' value='<?php echo "$this->viewData" ?>'>
+	<input type='password' name='password'>
+	<input type='submit' value='Send inn nytt passord'>
+	<input hidden name='token' value='<?php echo "$this->viewData" ?>'>
 </form>
-
 <?php include VIEW.'footer.php';?>
 
