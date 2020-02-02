c5df7f4656db1713cb982cfae4172e1c007cba29
taraldv <36439345+taraldv@users.noreply.github.com>
2020-02-02 16:43:28 +0100

    Update register.php
    endret til bootstrap alert

--- app/view/login/register.php
+++ app/view/login/register.php
@@ -1,4 +1,13 @@
 <?php include VIEW.'header.php';?>
+<?php 
+	if(isset($this->viewData[0])){
+		if($this->viewData[0]){
+			echo '<div id="errorMessageDiv" class="alert alert-success" role="alert">Sjekk epost for bekreftelse</div>';
+		} else {
+			echo '<div id="errorMessageDiv" class="alert alert-danger">Epost er i bruk, kontakt nettside administrator eller bruk en annen epost</div>';
+		}
+	}
+?>
 <h1 class="display-1 text-center">Trenings logg</h1>
 <img class="mx-auto d-block" src="/img/logo.png">
 <div class='container'>
@@ -16,14 +25,6 @@
 		</div>
 	</form>
 </div>
-<?php 
-	if(isset($this->viewData[0])){
-		if($this->viewData[0]){
-			echo '<h1>Sjekk epost for bekreftelse</h1>';
-		} else {
-			echo '<h1>Epost er i bruk, kontakt nettside administrator eller bruk en annen epost</h1>';
-		}
-	}
-?>
+
 <?php include VIEW.'footer.php';?>
 
