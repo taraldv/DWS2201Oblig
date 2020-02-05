86319fd09a6acefe9ac14c7baae8f2881bbd48d4
taraldv <36439345+taraldv@users.noreply.github.com>
2020-02-05 20:24:32 +0100

    Update header.php
    Mobile friendly

--- app/view/header.php
+++ app/view/header.php
@@ -5,6 +5,7 @@
 	<title>Webscience Oblig</title>
 	<link href="/main.css" type="text/css" media="all" rel="stylesheet">
 	<link href="/bootstrap.min.css" type="text/css" media="all" rel="stylesheet">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
 <script src='/script.js'></script>
