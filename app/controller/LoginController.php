9a27ddfdd03d3ab4ec2cc8f2dd35824a7447dbe9
taraldv <36439345+taraldv@users.noreply.github.com>
2020-02-06 00:17:28 +0100

    Update LoginController.php

--- app/controller/LoginController.php
+++ app/controller/LoginController.php
@@ -15,21 +15,6 @@ class LoginController extends Controller{
 		header("Location: /login");
 	}
 
-	/* GET request and form post request */
-	/*public function new_verify(){
-		$this->model('LoginModel');
-		$email = $_POST['email'];
-		//https://stackoverflow.com/questions/4356289/php-random-string-generator/31107425#31107425
-		$token = substr(md5(mt_rand()), 0, 100);
-		$mail = new Mail('Oblig.tarves.no epost verifisering',$email,'/login/verify');
-		$mail->setMailBody('Trykk på linken for å verifisere epost',$token,'Link');
-		$successfullyAdded = $this->model->setNewToken($email,$token);
-		if($successfullyAdded){
-			$mail->sendMail();
-		}	
-		$this->view('login'.'/'.'token.php',[$successfullyAdded]);
-		$this->view->render();	
-	}*/
 
 	/* POST request, sends email with password reset link. */
 	public function send_password_link(){	
